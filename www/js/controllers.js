915d9bbc00f7780b8031e128a1448e7abbca90c6
Slawek Kaczorowski <skaczorowski@opera.com>
2015-06-26 08:48:56 +0200

    * Added Photos fetchning form Photos service * Added tab-photos content * Added style for proper image showing

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,1 +1,23 @@
-angular.module('starter.controllers', []).controller('PhotosCtrl', function($scope) {}).controller('RatedCtrl', function($scope) {});
+angular.module('starter.controllers', []).controller('PhotosCtrl', function($scope, $ionicLoading, Photos, $q) {
+    $scope.photos = [];
+    $scope.photo;
+    var setPhotoOnStage = function() {
+        var defer = $q.defer();
+        Photos.getPhotos().then(function(images) {
+            $ionicLoading.show({
+                template: '<ion-spinner icon="android"></ion-spinner>'
+            });
+            var img = new Image();
+            img.onload = function() {
+                $ionicLoading.hide();
+                $scope.photos = images;
+                $scope.photo = $scope.photos[0];
+                defer.resolve();
+                $scope.$digest();
+            }
+            img.src = images[0].image.src;
+        });
+        return defer.promise;
+    };
+    setPhotoOnStage();
+}).controller('RatedCtrl', function($scope) {});
