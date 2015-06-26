ecb3adacb850dcd4da21602e46848dde1a2cc209
Slawek Kaczorowski <skaczorowski@opera.com>
2015-06-26 08:34:35 +0200

    Add Storage service

--- www/js/services.js
+++ www/js/services.js
@@ -15,4 +15,35 @@ angular.module('starter.services', []).factory('$localstorage', ['$window',
             }
         }
     }
-])
+]).factory('Storage', function($localstorage) {
+    return {
+        setUnrated: function(Images, offset) {
+            $localstorage.setObject('unratedPhotos', Images);
+            var lastOffset = parseInt($localstorage.get('lastOffset', 0));
+            $localstorage.set('lastOffset', lastOffset + offset);
+        },
+        getUnrated: function() {
+            return $localstorage.getObject('unratedPhotos');
+        },
+        getRated: function() {
+            return $localstorage.getObject('ratedPhotos');
+        },
+        getLastOffset: function() {
+            return $localstorage.get('lastOffset') || 0
+        },
+        rateImage: function(imgSrc, vote) {
+            unrated = this.getUnrated();
+            unrated = unrated.filter(function(el) {
+                return el.image.src !== imgSrc;
+            });
+            $localstorage.setObject('unratedPhotos', unrated);
+            var rated = $localstorage.getObject('ratedPhotos');
+            rated.push({
+                image: imgSrc,
+                vote: vote
+            });
+            $localstorage.setObject('ratedPhotos', rated);
+            return true;
+        }
+    }
+})
